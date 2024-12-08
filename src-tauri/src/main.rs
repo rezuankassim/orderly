fe649308962e6ecbf8949ba1506a77c0b5605b64
Rezuan Kassim <rezuankassim@hotmail.com>
2024-12-08 08:04:57 +0800

    testing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,18 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use tauri_plugin_sql::{Migration, MigrationKind};
+use printers::{get_printers};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn retrieve_printers() -> String {
+    let printers = get_printers();
+
+    // Prevent this error `Vec<Printer>` doesn't implement `std::fmt::Display`
+    let printers_vec = printers.iter().map(|printer| format!("{:?}", printer)).collect::<Vec<String>>();
+
+    // return json with array of printers
+    format!("{:?}", printers_vec)
 }
 
 fn main() {
@@ -27,7 +34,7 @@ fn main() {
                 .add_migrations("sqlite:mydatabase.db", migrations)
                 .build(),
         )
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![retrieve_printers])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
